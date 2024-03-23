32e61ff4e2c3a42e7b40be708cd0e0034a808a18
yangcs <yangcsky@163.com>
2024-03-23 10:41:22 +0800

    课程学习模块，选课然后进行学习

---
+++ xuecheng-plus-learning/xuecheng-plus-learning-service/src/main/java/com/yangcs/learning/mapper/XcChooseCourseMapper.java
@@ -0,0 +1,15 @@
+package com.yangcs.learning.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yangcs.learning.model.po.XcChooseCourse;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface XcChooseCourseMapper extends BaseMapper<XcChooseCourse> {
+
+}
---
+++ xuecheng-plus-learning/xuecheng-plus-learning-service/src/main/java/com/yangcs/learning/mapper/XcCourseTablesMapper.java
@@ -0,0 +1,15 @@
+package com.yangcs.learning.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.yangcs.learning.model.po.XcCourseTables;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface XcCourseTablesMapper extends BaseMapper<XcCourseTables> {
+
+}
